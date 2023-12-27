e182055987e55a89e94b99dbdfff5c95b8d9beb0
UsmanMERN <mu1363131@gmail.com>
2023-12-27 22:49:10 +0500

    learing proxy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 function App() {
+
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get("/api/jokes").then((res) => {
+      setJokes(res.data)
+    }).catch((e) => {
+      console.error("Something went wrong" + e.message);
+    })
+  })
+
   return (
-    <div className="App">
+    <div className="App d-flex justify-content-center align-item-center min-vh-100">
       <h2>This is practice app</h2>
+      <h5>jokes {jokes.length}</h5>
+      {jokes.map((j, i) => {
+        return <div key={i}>
+          <h2>{j.title}</h2>
+          <p>{j.content}</p>
+        </div>
+      })}
     </div>
   );
 }
